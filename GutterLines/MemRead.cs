2a5183efa89d6e657c1e89f06901adbe0f73ad21
Michael <ebirt@live.com>
2017-09-20 19:38:58 -0400

    new mem addr

--- GutterLines/MemRead.cs
+++ GutterLines/MemRead.cs
@@ -12,9 +12,9 @@ namespace GutterLines
 
         private int processNum;
         private Process curProcess;
-        private const int latAddress = 0x00D26694;
-        private const int lonAddress = 0x00D26698;
-        private const int nameAddress = 0x00D3CBE0;
+        private const int latAddress = 0x00D2DE74;
+        private const int lonAddress = 0x00D2DE78;
+        private const int nameAddress = 0x00D44630;
 
         public void GetProcess()
         {
@@ -52,14 +52,14 @@ namespace GutterLines
             }
         }
 
-        public static string ReadString(IntPtr process, IntPtr baseAddress)
+        private static string ReadString(IntPtr process, IntPtr baseAddress)
         {
             var buffer = new byte[8];
             ReadProcessMemory(process, baseAddress, buffer, 8, out IntPtr bytesRead);
             return System.Text.Encoding.Default.GetString(buffer).Replace("\0",string.Empty);
         }
 
-        public static int ReadInt(IntPtr process, IntPtr baseAddress)
+        private static int ReadInt(IntPtr process, IntPtr baseAddress)
         {
             var buffer = new byte[4];
             ReadProcessMemory(process, baseAddress, buffer, 4, out IntPtr bytesRead);
